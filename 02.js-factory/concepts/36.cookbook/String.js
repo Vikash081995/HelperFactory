be37f9bec75863243a81e2846a3cafa6b2a9ebd9
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-11-20 11:01:59 +0530

    refactoring

---
+++ 02.js-factory/concepts/36.cookbook/String.js
@@ -0,0 +1,8 @@
+//Checking if variable is a string
+if(typeof variable === 'string' || variable instanceof String) {
+    console.log("The variable is a string.");
+} else {
+    console.log("The variable is not a string.");
+}
+
+//
